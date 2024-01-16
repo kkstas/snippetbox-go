31cd0e2ead371280cc56eb677f0d9a5c9e4f1382
kkstas <stasiakkamilkrzysztof@gmail.com>
2024-01-16 20:12:25 +0100

    User login

--- internal/models/users.go
+++ internal/models/users.go
@@ -45,7 +45,26 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(eamil, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+	err := m.DB.QueryRow(stmt, eamil).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		}
+		return 0, err
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			return 0, ErrInvalidCredentials
+		}
+		return 0, err
+	}
+	return id, nil
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
